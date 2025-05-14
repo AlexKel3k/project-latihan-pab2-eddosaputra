493556f2e2be10eeecd8fe37646032bdfa6c33e8
dedimanaluu <161033339+eddosaputra@users.noreply.github.com>
2025-05-14 19:34:32 +0700

    menambahkan  screen baru edit post my post

--- fasum_app/README.md
+++ fasum_app/README.md
@@ -3,6 +3,7 @@ Eddo Saputra 232725007
 firebase login
 flutterfire configure
 dart pub global activate flutterfire_cli
+
 npm install -g firebase-tools
 
 dependency_overrides:
--- fasum_app/lib/main.dart
+++ fasum_app/lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:fasum_app/screens/home_screen.dart';
-import 'package:fasum_app/screens/sign_in_screen.dart';
 import 'package:fasum_app/screens/splash_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
